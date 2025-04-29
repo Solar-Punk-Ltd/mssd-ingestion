fb687058d0b478c212cb424c6dee6d128d09413b
Levente Kiss <levente.kiss@solarpunk.buzz>
2025-04-29 12:55:52 +1000

    chore: template string app_home

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '32GB',
+      max_memory_restart: '32G',
       time: true,
     },
   ],
